a9e33b1bba127f0acda212efeec553171db9bab6
AJ Burns <aj501173@gmail.com>
2022-03-18 22:16:55 -0600

    adding update

--- circle.py
+++ circle.py
@@ -1,6 +1,6 @@
 import pygame
 class circle:
-    def __init__(self, pos=(0.0, 0.0), velocity=(0.0, 0.0), mass=0, static=False, netForce=0, radius=100):
+    def __init__(self, pos=(0.0, 0.0), velocity=(0.0, 0.0), mass=1.0, static=False, netForce=0.0, radius=100.0):
          self.pos = pos
          self.velocity = velocity
          self.mass = mass
@@ -10,6 +10,11 @@ class circle:
     
     def applyforce(self, forceVector):
         self.netForce += forceVector
+
+    def update(self):
+        acceleration = self.netForce/self.mass
+        self.velocity += acceleration
+        
 
     def draw(self):
        # pygame.draw.circle(Surface, color, self.pos, radius, width=0)

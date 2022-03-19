6f6b96e423e49dbd99d851a50bc24d47be0d26b3
Joshua <joshua.hamby@usu.edu>
2022-03-19 07:53:24 -0600

    changes color on collision

--- simulator.py
+++ simulator.py
@@ -2,17 +2,23 @@ import pygame
 from line import line
 from circle import circle
 from helper import *
+import random
 
 
 class simulator:
     def __init__(self):
         self.physics_objects = []
-        self.physics_objects.append(circle(pos=(50.0, 50.0), velocity=(4.0, 4.0), mass=1.0, static=False, netForce=(0.0, 6.0), radius=50.0))
+        self.physics_objects.append(circle(pos=(50.0, 50.0), velocity=(0.2, 0.0), mass=1.0, static=False, netForce=(1.0, 1.0), radius=50.0))
         self.physics_objects.append(line(startpos=(1.0, 0.0), endpos=(0.0, 700.0), static=True))
         self.physics_objects.append(line(startpos=(0.0, 701.0), endpos=(1100.0, 700.0), static=True))
         self.physics_objects.append(line(startpos=(1101.0, 0.0), endpos=(1100.0, 700.0), static=True))
         self.physics_objects.append(line(startpos=(0.0, 1.0), endpos=(1100.0, 0.0), static=True))
-        self.physics_objects.append(line(startpos=(0.0, 1.0), endpos=(1100.0, 0.0), static=True))
+        self.colors=[(255,127,36), (238,118,33), (205,102,29), (139,69,19), (61,89,171), (61,145,64), (128,138,135), (255,127,80), (
+255,114,86), (238,106,80), (205,91,69), (139,62,47), (100,149,237), (255,248,220), (238,232,205), (205,200,177), (139,136,120
+), (220,20,60), (0,238,238), (0,205,205), (0,139,139), (184,134,11), (255,185,15), (238,173,14), (205,149,12), (139,101,8)
+, (169,169,169), (0,100,0), (189,183,107), (85,107,47), (202,255,112), (188,238,104), (162,205,90), (110,139,61), (255,140,0
+), (255,127,0), (238,118,0)]
+       
 
     def render(self, screen):
         for physics_object in self.physics_objects:
@@ -52,6 +58,7 @@ class simulator:
                     wall = Tadd(object2.endpos, scalar_mult(-1, object2.startpos))
                     normal = normalize(wall)
                     object1.velocity = Tadd(object1.velocity, scalar_mult(-2 * dot_product(normal, object1.velocity), normal))
+                    object1.color = self.colors[random.randint(0,len(self.colors)-1)]
 
     def add_physics_object(self, physics_object):
         self.physics_objects.append(physics_object)

f21d984d7fd0dfdb7fc7599cbf2e10859f91b87c
AJ Burns <aj501173@gmail.com>
2022-03-18 23:29:29 -0600

    rando

--- circle.py
+++ circle.py
@@ -3,7 +3,7 @@ from helper import *
 
 
 class circle:
-    def __init__(self, pos=(0.0, 0.0), velocity=(0.0, 0.0), mass=1.0, static=False, netForce=0.0, radius=100.0):
+    def __init__(self, pos=(0.0, 0.0), velocity=(0.0, 0.0), mass=1.0, static=False, netForce=(0.0, 0.0), radius=100.0):
         self.pos = pos
         self.velocity = velocity
         self.mass = mass
@@ -18,10 +18,11 @@ class circle:
 
         self.pos = Tadd(self.velocity, self.pos)
 
+
         acceleration = TdivF(self.netForce, self.mass)
         self.velocity = Tadd(self.velocity, acceleration)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 255, 255), self.pos, self.radius)
+        pygame.draw.circle(screen, (255, 255, 255), tuple(map(int, self.pos)), int(self.radius))
 
 

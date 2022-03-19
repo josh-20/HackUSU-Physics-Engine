6dc199f3c22fda1939da0c90b4593e3a9809eb48
Albin Dittli <A02365308@aggies.usu.edu>
2022-03-18 21:16:41 -0600

    more engine stuff

--- simulator.py
+++ simulator.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-def simulator():
+class simulator:
     def __init__(self):
         self.physics_objects = []
         pass
@@ -9,7 +9,7 @@ def simulator():
     def update(self):
         pass
 
-    def render(self):
+    def render(self, screen):
+        pygame.draw.circle(screen, (123, 45, 98), (57, 68), 25)
         for physics_object in self.physics_objects:
             physics_object.draw()
-            pygame.draw.circle(3, 0xFF0000, )
